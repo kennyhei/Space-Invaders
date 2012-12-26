039a93b2dc20cedcb5ce3f77b2b1fb0335ab41a8
Kenny Heinonen <kenny_heinonen@hotmail.com>
2012-12-26 14:53:00 +0200

    deleted useless code

--- spaceinvaders/src/main/webapp/js/code.js
+++ spaceinvaders/src/main/webapp/js/code.js
@@ -335,19 +335,6 @@ $(document).ready(function() {
         eventInformation.preventDefault();
     });
     
-    $.fn.extend({ 
-        disableSelection : function() { 
-            this.each(function() { 
-                this.onselectstart = function() {
-                    return false;
-                }; 
-                this.unselectable = "on"; 
-                $(this).css('-moz-user-select', 'none'); 
-                $(this).css('-webkit-user-select', 'none'); 
-            }); 
-        } 
-    });
-    
     $("#right").swipe({
         swipeStatus:function(event, phase, direction, distance, duration, fingerCount) {
             if (phase == "start" || phase == "move")
